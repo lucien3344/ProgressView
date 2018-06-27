3bcefe8a181d2b8c1a3bf9b08a5cfaf983c6655c
Administrator <lsh_2012@qq.com>
2018-06-27 18:13:10 +0800

    第二次提交ProgressView

--- app/src/main/java/com/lucien3344/progress_view/MainActivity.kt
+++ app/src/main/java/com/lucien3344/progress_view/MainActivity.kt
@@ -2,11 +2,17 @@ package com.lucien3344.progress_view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        circleProgressBar.setProgress(56f, "分");
+        progresline_sys.setDoProgress("说一说", 82.0f, "%");
+        progresline_xyx.setDoProgress("学一学", 25.6f, "%");
+
     }
 }
